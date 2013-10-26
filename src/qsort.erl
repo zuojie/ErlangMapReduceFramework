5addb3d011cc91fd64779f6b1f0d3c097eea014d
zuojiepeng <pengzuojie@gmail.com>
2013-10-26 01:21:37 -0700

    Update qsort.erl

--- src/qsort.erl
+++ src/qsort.erl
@@ -1,10 +1,16 @@
 -module(qsort).                                                                    
 -compile(export_all).                                                              
                                                                                    
+my_reduce([]) ->                                                                   
+    [];                                                                            
+                                                                                   
+my_reduce(OutDat) ->                                                               
+    OutDat.                                                                        
+                                                                                   
 qsort([]) -> [];                                                                   
 qsort([Pivot]) -> [Pivot];                                                         
 qsort([Pivot | Rest]) ->                                                           
     L = [X || X <- Rest, X =< Pivot],                                              
     R = [X || X <- Rest, X > Pivot],                                               
-    [SortL, SortR] = mprd_master:map(fun qsort/1, [L, R], false),                  
-    SortL ++ [Pivot] ++ SortR.
+    [SortL, SortR] = mprd_master:start(fun qsort/1, fun my_reduce/1, [L, R]),   
+    SortL ++ [Pivot] ++ SortR.                         
